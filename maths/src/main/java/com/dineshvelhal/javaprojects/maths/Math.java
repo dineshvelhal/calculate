3373cd7eb9991559593a2a469fbc8c2ebb06018f
Dinesh Velhal <dinesh.velhal@gmail.com>
2019-04-27 23:23:24 +0530

    Update Math.java

--- maths/src/main/java/com/dineshvelhal/javaprojects/maths/Math.java
+++ maths/src/main/java/com/dineshvelhal/javaprojects/maths/Math.java
@@ -4,7 +4,8 @@ package com.dineshvelhal.javaprojects.maths;
 /**
  * @author Dinesh_Velhal (dinesh.velhal at gmail)
  * 
- * A dummy Math class used for testing the Jenkins pipeline builds
+ * A dummy Math class used for testing the Jenkins pipeline builds.
+ * 
  *
  */
 public class Math 
