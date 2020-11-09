e0df3bfcd75d8514f935f9b0de17de84dd9b0462
Dinesh Velhal <dinesh.velhal@gmail.com>
2020-11-09 15:44:29 -0600

    added basic testng tests

---
+++ maths/src/test/java/com/dineshvelhal/javaprojects/maths/tests/SmokeTests.java
@@ -0,0 +1,34 @@
+package com.dineshvelhal.javaprojects.maths.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class SmokeTests {
+  @Test(description = "Login Test")
+  public void application_login_test() {
+	  
+  }
+  
+  @Test(description = "User Registration Test")
+  public void user_registration_test() {
+	  
+  }
+  
+  @Test(description = "User cancellation Test")
+  public void user_cancellation_test() {
+	  
+  }
+  
+  
+  @BeforeTest
+  public void initiate_browser() {
+	  
+  }
+
+  @AfterTest
+  public void close_browser() {
+	  
+  }
+
+}
