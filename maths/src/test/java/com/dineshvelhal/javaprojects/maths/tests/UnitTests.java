d7e63992dcf1a3cbe5172d96b2e4dbe947f047ec
Dinesh Velhal <dinesh.velhal@gmail.com>
2020-11-09 19:23:57 -0600

    full coverage

--- maths/src/test/java/com/dineshvelhal/javaprojects/maths/tests/UnitTests.java
+++ maths/src/test/java/com/dineshvelhal/javaprojects/maths/tests/UnitTests.java
@@ -13,32 +13,33 @@ import org.testng.annotations.AfterTest;
 
 public class UnitTests {
 	Maths math;
-	
-	/*
-	 * @Test(description = "Addition Test") public void addTest() {
-	 * System.out.println("@Test Inside addTest");
-	 * 
-	 * assertEquals(5, math.add(2, 3)); }
-	 */
-	
+
+
+	@Test(description = "Addition Test") public void addTest() {
+		System.out.println("@Test Inside addTest");
+
+		assertEquals(5, math.add(2, 3)); 
+	}
+
+
 	@Test(description = "Subtraction Test")
 	public void subtractTest() {
 		System.out.println("@Test Inside subtractTest");
-		
+
 		assertEquals(5, math.subtract(7, 2));
 	}
-	
+
 	@Test(description = "Multiplication Test")
 	public void multiplyTest() {
 		System.out.println("@Test Inside multiplyTest");
-		
+
 		assertEquals(15, math.multiply(5, 3));
 	}
-	
+
 	@Test(description = "Division Test")
 	public void divideTest() {
 		System.out.println("@Test Inside divideTest");
-		
+
 		assertEquals(5, math.divide(20, 4));
 		assertNotEquals(5.5, math.divide(11, 2)); // to check it's integer division
 	}
