b3e82340c262bba90a5a0f52156cbe88273885fe
Dinesh Velhal <dinesh.velhal@gmail.com>
2020-11-17 14:10:56 -0600

    error in dev branch

--- maths/ReleaseTests/src/test/java/com/dineshvelhal/javaprojects/maths/tests/IntegrationTests.java
+++ maths/ReleaseTests/src/test/java/com/dineshvelhal/javaprojects/maths/tests/IntegrationTests.java
@@ -18,7 +18,7 @@ public class IntegrationTests {
 	public void addTest() {
 		System.out.println("@Test Inside addTest");
 		
-		assertEquals(5, math.add(2, 3));
+		assertEquals(5, math.add(2, 6));
 	}
 	
 	@Test(description = "Subtraction Test")
